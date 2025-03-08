8e1afe29c66b4a30cfc6a07fa95e74dcdaadbc3f
TuongRoth <rothtrancattuong0703@gmail.com>
2025-03-08 09:20:35 +0200

    "Fixed footer styles and improved CSS structure"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home'; 
 import Cart from './pages/Cart/Cart';  // Import component Cart
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';  // Import component PlaceOrder
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />  {/* Thêm Route cho Cart */}
-        <Route path="/order" element={<PlaceOrder />} />  {/* Thêm Route cho PlaceOrder */}
-      </Routes>
-    </div>
+    <> <div className='app'>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />  {/* Thêm Route cho Cart */}
+      <Route path="/order" element={<PlaceOrder />} />  {/* Thêm Route cho PlaceOrder */}
+    </Routes>
+  </div>
+<Footer/>  </>
+   
   );
 };
 
